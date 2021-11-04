8297d84af8ae5cb0d92725e14f3f1d4be338bc67
Matheus silva <teus_26@hotmail.com>
2021-11-04 16:31:36 -0300

    src

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-app id="inspire">
+    <!-- <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      
+    </v-navigation-drawer> -->
+
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Keep Vuetify
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Lista de tarefas
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to='item.to'
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar> -->
+
+    <v-app-bar 
+      app
+      prominent
+      color="#fcb69f"
+      dark
+      src='https://picsum.photos/1920/1000?random'
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+        ></v-img>
+      </template>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({ 
+      drawer: null,
+      items: [
+          { title: 'Tarefas', icon: 'mdi-view-dashboard', to:'/' },
+          { title: 'Sobre', icon: 'mdi-image', to:'/sobre' }
+        ],
+    }),
+  }
+</script>
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-list
+        flat
+        subheader
+      >
+        <!-- <v-subheader>General</v-subheader> -->
+        <v-list-item-group
+          v-model="settings"
+          multiple
+          active-class=""
+        >
+        <Tarefa />
+
+        </v-list-item-group>
+      </v-list>
+  </div>
+</template>
+
+<script>
+  import Tarefa from '../components/tarefas/Tarefa.vue'
+
+  export default {
+    name: 'Tarefas',
+
+    components: {
+      Tarefa
+    },
+  }
+</script>
