1c8b30a36098653f57085256097e0a2f1cef9551
Matheus silva <teus_26@hotmail.com>
2021-11-16 16:25:26 -0300

    add modal

---
+++ src/components/tarefas/TarefaMenu.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          v-bind="attrs"
+          v-on="on"
+          icon>
+        <v-icon>
+          mdi-dots-vertical
+        </v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click="item.click()"
+        >
+          <v-icon left>{{ item.icon }}</v-icon>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <ModalEditar />
+  </div>
+</template>
+
+<script>
+  import ModalEditar from '../Modal/ModalEditar.vue'
+
+export default {
+  name: 'TarefaMenu',
+  components: { 
+    ModalEditar 
+  },
+  data: () => ({
+      items: [
+        { 
+          icon:'mdi-pencil', 
+          title: 'Editar',
+          click() {
+            console.log('Editar')
+          }
+        },
+        { 
+          icon:'mdi-trash-can', 
+          title: 'Excluir',
+          click() {
+            console.log('Excluir')
+          }
+        },
+      ],
+    }),
+}
+</script>
+
+<style>
+
+</style>
