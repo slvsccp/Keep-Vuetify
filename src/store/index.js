5a6416bf738846640b478c60f8e26b6f41d62b31
Matheus silva <teus_26@hotmail.com>
2021-11-16 14:58:44 -0300

    state e mutations

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tarefas: [{ id: 1, titulo: 'Ir ao mercado', concluido: false }]
   },
   mutations: {
+    adicionaTarefa(state, titulo) {
+      if (titulo) {
+        state.tarefas.push({
+          id: new Date().getTime(),
+          titulo,
+          concluido: false
+        })
+        //this.campoInput = null
+      }
+    },
+    removeTarefa(state, id) {
+      state.tarefas = state.tarefas.filter(tarefa => tarefa.id !== id)
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {}
 })
