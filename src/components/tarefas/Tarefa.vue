81c48b9aac9b7926cd77bb9ea7398bd99740f861
Matheus silva <teus_26@hotmail.com>
2021-11-16 14:58:11 -0300

    tarefas

--- src/components/tarefas/Tarefa.vue
+++ src/components/tarefas/Tarefa.vue
@@ -1,21 +1,45 @@
 <template>
   <div>
-    <v-list-item>
-      <template v-slot:default="{ active }">
+    <v-list-item
+    :class="{'blue lighten-4':tarefa.concluido}"
+    @click="tarefa.concluido = !tarefa.concluido"
+    >
+      <template v-slot:default="{  }">
         <v-list-item-action>
-          <v-checkbox :input-value="active"></v-checkbox>
+          <v-checkbox :input-value="tarefa.concluido"></v-checkbox>
         </v-list-item-action>
+
         <v-list-item-content>
-          <v-list-item-title>Notifications</v-list-item-title>
+          <v-list-item-title
+          :class="{'text-decoration-line-through':tarefa.concluido}"
+          >{{ tarefa.titulo }}</v-list-item-title>
         </v-list-item-content>
+
+        <v-list-item-action>
+          <v-btn 
+          icon
+          @click.stop='handleRemoveTarefa(tarefa.id)'
+          >
+            <v-icon color="grey lighten-1">mdi-trash-can</v-icon>
+          </v-btn>
+        </v-list-item-action>
+
       </template>
     </v-list-item>
+
+    <v-divider></v-divider>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Tarefa'
+  name: 'Tarefa',
+  props: ['tarefa'],
+  methods: {
+    handleRemoveTarefa(id) {
+      this.$store.commit('removeTarefa', id)
+    }
+  }
 }
 </script>
 
